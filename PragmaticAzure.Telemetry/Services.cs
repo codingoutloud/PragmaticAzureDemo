12373eb032ca8fad53c2c58da12f4863d161e3f2
Bill Wilder <codingoutloud@gmail.com>
2014-07-30 12:51:47 -0400

    tweakage

---
+++ PragmaticAzure.Telemetry/Services.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PragmaticAzure.Telemetry
+{
+    public enum Services
+    {
+        PragmaticAzureApplication = 1,
+        PragmaticAzureInvoicingService
+    }
+
+    // alternative style:
+    /*
+    public static class Services
+    {
+        public static readonly string PragmaticAzureApplication = "Pragmatic Azure App";
+    }
+    */
+}
